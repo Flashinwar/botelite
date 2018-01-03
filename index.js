759a163db962af03acde4d10b21a025b5770a0ef
Flashinwar <35080421+Flashinwar@users.noreply.github.com>
2018-01-04 00:02:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+  console.log("Je suis connecté !")
+})
+
+bot.login('k7abhPKuKUjq2tRVTQMGfq8yxAfl9v_A')
